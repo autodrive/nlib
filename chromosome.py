69c632a796902c77fc1515a3e6d7b8bbf50d277e
KangWon LEE <kangwon.lee@kpu.ac.kr>
2017-04-05 11:49:41 +0900

    added comments for Chromosome class

--- chromosome.py
+++ chromosome.py
@@ -14,20 +14,37 @@ class Chromosome(object):
     # methods
     def __init__(self, father=None, mother=None):
         if not father or not mother:
+            # if one or both of parents not given initialize with random
             self.dna = [random.choice(self.alphabet) for i in range(self.size)]
         else:
+            # if both parents given, combine father's and mother's genes and mutate
             father_half = father.dna[0:(self.size // 2)]
             mother_half = mother.dna[(self.size // 2):]
             self.dna = father_half + mother_half
+            # end combining genes
+
+            # mutate gene
             for mutation in range(self.mutations):
                 self.dna[random.randint(0, self.size - 1)] = random.choice(self.alphabet)
 
+    # could be overridden
     def fitness(self, target):
+        """
+        count number of genes of same loation same value
+        :param target:
+        :return:
+        """
         ones_list = [1 for i, c in enumerate(self.dna) if c == target.dna[i]]
         result = sum(ones_list)
         return result
 
     def __lt__(self, other):
+        """
+        list sort method needs this
+
+        :param other:
+        :return:
+        """
         return self.dna.__lt__(other.dna)
 
 
